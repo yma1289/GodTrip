79ad792f5e654a6078fee8718be0b7e3d50e685f
limjaehun <jqrkp7077@naver.com>
2023-06-14 19:52:20 +0900

    member

--- src/main/java/kr/co/godtrip/member/MemberDAO.java
+++ src/main/java/kr/co/godtrip/member/MemberDAO.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,29 @@ public class MemberDAO {
 		public MemberDAO() {
 			System.out.println("----MemberDAO() end");
 		}
+		
 		@Autowired
 		SqlSession sqlSession;
-		
+		//회원가입
 		public void insert(MemberDTO dto) {
 			sqlSession.insert("member.insert",dto);
 		}
+		
+		//중복체크 id, email
+		public int duplicateId(String id) {
+			return sqlSession.selectOne("member.duplicateId",id);
+		}
+		
+		public int duplicateemail(String email) {
+			return sqlSession.selectOne("member.duplicateEmail",email);
+		}
+		
+		//로그인
+		public MemberDTO loginProc(String id, String passwd) {
+		    Map<String, String> paramMap = new HashMap<>();
+		    paramMap.put("id", id);
+		    paramMap.put("passwd", passwd);
+		    return sqlSession.selectOne("member.loginProc", paramMap);
+		}
+		
 }
